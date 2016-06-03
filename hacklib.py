9f75df56f3f9cfb03304056b066ddc0690156f13
Leon Li <leontosy@gmail.com>
2016-06-03 09:12:46 -0700

    Added AuthClient for most HTTP/HTTPS login pages

--- hacklib.py
+++ hacklib.py
@@ -41,6 +41,89 @@ class CamHacker:
         print 'Security Cam URL: ' + data_list[0] + '\n' + 'Username: ' + data_list[1] + '\n' + 'Password: ' + data_list[2]
         return
 
+class AuthClient:
+    '''Universal login tool for most login pages as well as HTTP Basic Authentication.
+    '''
+
+    def __init__(self):
+        self.url = ''
+        self.username = ''
+        self.password = ''
+
+    def _get_login_type(self):
+        try:
+            # Attempts to urlopen target URL without exception
+            data = urllib2.urlopen(self.url)
+            return 'FORM'
+        except Exception, e:
+            print str(e)
+            if 'error 401' in str(e).lower():
+                return 'BA'
+            if 'timed out' in str(e).lower():
+                return 'TO'
+            
+    def _login_mechanize(self):
+        try:
+            import mechanize
+        except:
+            raise Exception('Please install mechanize module before continuing. (pip install mechanize)')
+        # Sets up common input names/ids and creates instance of mechanize.Browser()
+        userfields = ['user', 'username', 'usr', 'email', 'name', 'login', 'userid', 'userid-input', 'player']
+        passfields = ['pass', 'password', 'passwd', 'pw']
+        br = mechanize.Browser()
+        br.set_handle_robots(False)
+        br.set_handle_refresh(False)
+        br.addheaders = [('User-agent', 'googlebot')]
+        # Opens URL and lists controls
+        response = br.open(self.url)
+        loginurl = response.geturl()
+        br.form = list(br.forms())[0]
+        username_control = ''
+        password_control = ''
+        # Locates username and password controls, and submits login info
+        for control in br.form.controls:
+            if control.name in userfields or control.id in userfields: username_control = control
+            if control.name in passfields or control.id in passfields: password_control = control
+        username_control.value = self.username
+        password_control.value = self.password
+        response = br.submit()
+        # Returns response if the URL is changed. Assumes login failure if URL is the same
+        if response.geturl() != loginurl:
+            return response.read()
+        else:
+            return False
+
+    def _login_BA(self):
+        try:
+            # Creates a PasswordMgr instance
+            passmanager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+            passmanager.add_password(None, self.url, self.username, self.password)
+            # Creates an auth handling object and builds it with opener
+            auth = urllib2.HTTPBasicAuthHandler(passmanager)
+            opener = urllib2.build_opener(auth)
+            response = opener.open(self.url, timeout=8)
+            response.close()
+            return True
+        except Exception, e:
+            print str(e)
+            if 'Error 401' in str(e):
+                return False
+            
+    def login(self, url, username, password):
+        self.url = url
+        self.username = username
+        self.password = password
+        # ascertain the type of login page given by url
+        logintype = self. _get_login_type()
+        if logintype == 'BA':
+            # attempts to login with BA method and return True
+           return self._login_BA()
+        if logintype == 'TO':
+            print 'Request timed out.'
+            return False
+        if logintype == 'FORM':
+            return self._login_mechanize()
+
 class DOSer:
     '''Hits a host with packets from multiple threads.
     '''
@@ -53,7 +136,7 @@ class DOSer:
         self.time_length = 1
 
     def _attack(self, target, port=80):  
-        #pid = os.fork()
+        # Sends GET requests for time_length duration
         while int(time.time()) < self.start_time + self.time_length:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(1)
@@ -77,13 +160,14 @@ class DOSer:
         self.start_time = int(time.time())
         self.time_length = duration
         if payload != 'default': self.payload = payload
+        # Creates queue to hold each thread
         self.q = Queue()
         print '> Launching ' + str(threads) + ' threads for ' + str(duration) + ' seconds.'
-        for x in range(threads):
+        for i in range(threads):
             t = threading.Thread(target=self._threader)
             t.daemon = True
             t.start()
-
+        # Adds workers to queue
         for worker in range(0, threads):
             self.q.put(worker)
 
@@ -104,6 +188,7 @@ class PortScanner:
     def _portscan(self, port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(self.timeout)
+        # Tries to establish a connection to port, and append to list of open ports
         try:
             con = s.connect((self.IP,port))
             response = s.recv(1024)
@@ -113,9 +198,9 @@ class PortScanner:
                     print 'Port', str(port) + ':'
                     print response
             s.close()
-
+        # If the connection fails, tries to establish HTTP connection
         except Exception, e:
-            httplist = [80, 443, 2082, 2083]
+            httplist = [80, 443, 1900, 2082, 2083, 8080, 8443]
             if port in httplist:
                 try:
                     s.send('GET HTTP/1.1 \r\n')
@@ -148,12 +233,13 @@ class PortScanner:
         self.IP = IP
         self.port_range = port_range
         self.timeout = 1
+        # Creates queue to hold each thread
         self.q = Queue()
         for x in range(30):
             t = threading.Thread(target=self._threader)
             t.daemon = True
             t.start()
-            
+        # Adds workers to queue
         for worker in range(1, port_range):
             self.q.put(worker)
 
@@ -162,10 +248,11 @@ class PortScanner:
 def getIP(host):
     return socket.gethostbyname(host)
 
-def send(IP, port, message):
+def send(IP, port, message, keepalive = False):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection = s.connect((IP, port))
     s.send(message)
     response = s.recv(1024)
-    s.close()
+    if not keepalive:
+        s.close()
     return response
