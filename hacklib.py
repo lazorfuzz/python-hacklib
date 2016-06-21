359adcb8e706170503a73566edd6e25a15a6987d
Leon Li <leontosy@gmail.com>
2016-06-20 19:40:28 -0700

    Update hacklib.py

--- hacklib.py
+++ hacklib.py
@@ -19,7 +19,7 @@ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFT
 OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.'''
 
 import socket, httplib, threading, time, urllib2
-from queue import Queue
+from multiprocessing import Queue
 
 class FTPAuth:
     '''FTP login and command handler.
